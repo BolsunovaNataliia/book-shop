14aa402342078430e2493a00375097c61dad5c0f
Nataliia <gzodchie@gmail.com>
2023-12-11 15:06:09 +0200

    added liquibase support and soft delete concept

--- src/main/java/mate/academy/bookshop/repository/BookRepository.java
+++ src/main/java/mate/academy/bookshop/repository/BookRepository.java
@@ -1,13 +1,10 @@
 package mate.academy.bookshop.repository;
 
-import java.util.List;
-import java.util.Optional;
 import mate.academy.bookshop.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BookRepository {
-    Book save(Book book);
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 
-    Optional<Book> findById(Long id);
-
-    List<Book> findAll();
 }
