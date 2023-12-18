de4989ad4f12b407a8aa57e925d5d49efe4d9d2b
Nataliia <gzodchie@gmail.com>
2023-12-18 14:06:31 +0200

    added constants

--- src/main/java/mate/academy/bookshop/repository/book/spec/PriceSpecificationProvider.java
+++ src/main/java/mate/academy/bookshop/repository/book/spec/PriceSpecificationProvider.java
@@ -8,13 +8,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PriceSpecificationProvider implements SpecificationProvider<Book> {
+    private static final String FIELD_PRICE = "price";
+
     @Override
     public String getKey() {
         return "price";
     }
 
     public Specification<Book> getSpecification(String[] params) {
-        return (root, query, criteriaBuilder) -> root.get("price")
+        return (root, query, criteriaBuilder) -> root.get(FIELD_PRICE)
                 .in(Arrays.stream(params).toArray());
     }
 }
