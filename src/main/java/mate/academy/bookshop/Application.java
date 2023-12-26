eb82240a6e312a073cab50fcabab6a6bcab75929
Nataliia <gzodchie@gmail.com>
2023-12-26 17:10:52 +0200

    rename application class

--- src/main/java/mate/academy/bookshop/Application.java
+++ src/main/java/mate/academy/bookshop/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookShopApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(BookShopApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
